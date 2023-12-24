82b5a3284c15993d3b459816d1ee5daebc432556
Philip Larsson <philiplarsson@mail.com>
2023-12-24 12:39:24 +0100

    Added some js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function () {
+    
+});
+
+$(".type-button-collapse").click(function () {
+    if($(".type-dropdown-menu").is(':visible'))
+        $(".type-dropdown-menu").hide();
+    else
+        $(".type-dropdown-menu").show();
+});
